ed1bb9cb17caa453fd53754874dc4f94f8eee2ca
xztaityozx <taityonohibi@gmail.com>
2019-01-02 18:47:12 +0900

    非ASCIIにも対応したつもりだよ あとテスト追加したよ

---
+++ noc/Test/UnitTest1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using noc;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Test {
+    public class UnitTest1 {
+        [Fact]
+        public void GetModeSmall() {
+            var nabe = new Nabe();
+
+            for (var c = 'a'; c <= 'z'; c++) {
+                Assert.Equal(Nabe.Block.Mode.Small, nabe.GetMode($"{c}"));
+            }
+        }
+
+        [Fact]
+        public void GetModeBig() {
+            var nabe = new Nabe();
+
+            for (var c = 'A'; c <= 'Z'; c++) {
+                Assert.Equal(Nabe.Block.Mode.Big, nabe.GetMode($"{c}"));
+            }
+        }
+
+        [Fact]
+        public void GetModeSymbol() {
+            var nabe = new Nabe();
+
+            foreach (var s in new[] {
+                "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+",
+                ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@",
+                "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", " "
+            }) {
+
+                Assert.Equal(Nabe.Block.Mode.Symbol, nabe.GetMode(s));
+            }
+        }
+
+        [Fact]
+        public void GetModeBase64() {
+            var nabe = new Nabe();
+
+            for (var c = 'あ'; c <= 'ん'; c++) {
+                Assert.Equal(Nabe.Block.Mode.Base64, nabe.GetMode($"{c}"));
+            }
+        }
+    }
+
+    public class UnitTest2 {
+        [Fact]
+        public void Encode001() {
+            const string src = "abc";
+            Assert.Equal("辺辺󠄀辺󠄁辺󠄂", new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode002() {
+            const string src = "ABC";
+            Assert.Equal("邉邉󠄀邉󠄁邉󠄂", new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode003() {
+            const string src = "123";
+            Assert.Equal("邊邊󠄐邊󠄑邊󠄒", new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode004() {
+            const string src = "あいう";
+            Assert.Equal("渡邊邊󠄓邊󠄓邉邉󠄆邉󠄂邊邊󠄓邊󠄓邉邉󠄆邉󠄄邊邊󠄓邊󠄓邉邉󠄆邉󠄆渡", new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode005() {
+            var src = string.Join("", Enumerable.Range(0, 26).Select(x => (char) ('a' + x)));
+            Assert.Equal("辺辺󠄀辺󠄁辺󠄂邉󠄀邉󠄁邉󠄂邉󠄃邉󠄄邉󠄅邉󠄆邉󠄇邉󠄈邉󠄉邉󠄊邉󠄋邉󠄌邉󠄍邉󠄎邉󠄏邉󠄐邉󠄑邉󠄒邉󠄓邉󠄔邉󠄕邉󠄖",
+                new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode006() {
+            var src = string.Join("", Enumerable.Range(0, 26).Select(x => (char) ('A' + x)));
+            Assert.Equal("邉邉󠄀邉󠄁邉󠄂邉󠄃邉󠄄邉󠄅邉󠄆邉󠄇邉󠄈邉󠄉邉󠄊邉󠄋邉󠄌邉󠄍邉󠄎邉󠄏邉󠄐邉󠄑邉󠄒邉󠄓邉󠄔邉󠄕邉󠄖邉󠄗邉󠄘邉󠄙",
+                new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode007() {
+            const string src = "0123456789";
+            Assert.Equal("邊邊󠄏邊󠄐邊󠄑邊󠄒邊󠄓邊󠄔辺󠄀辺󠄁辺󠄂邉󠄀", new NabeEncoder().Encode(src));
+        }
+
+        [Fact]
+        public void Encode008() {
+            const string src = "こんにちは!これはnocコマンドのTestですよ！.";
+            Assert.Equal(
+                "渡邊邊󠄓邊󠄓邉邉󠄆邉󠄓邊邊󠄓邊󠄓邉邉󠄊邉󠄓邊邊󠄓邊󠄓邉邉󠄆辺邉󠄎邊邊󠄓邊󠄓邉邉󠄆辺邉󠄄邊邊󠄓邊󠄓邉邉󠄆辺邉󠄒渡邊邊󠄀渡邊邊󠄓邊󠄓邉邉󠄆邉󠄓邊邊󠄓邊󠄓邉邉󠄊邉󠄌邊邊󠄓邊󠄓邉邉󠄆辺邉󠄒渡辺邉󠄊邉󠄋辺󠄂渡邊邊󠄓邊󠄓邉邉󠄊辺邉󠄖邊邊󠄓邊󠄓邉邉󠄎辺邉󠄁邊邊󠄓邊󠄓邉邉󠄎辺邉󠄖邊邊󠄓邊󠄓邉邉󠄎邉󠄉邊邊󠄓邊󠄓邉邉󠄆辺邉󠄑渡邉邉󠄓辺邉󠄁邉󠄏邉󠄐渡邊邊󠄓邊󠄓邉邉󠄆辺邉󠄊邊邊󠄓邊󠄓邉邉󠄆邉󠄙邊邊󠄓邊󠄓邉邉󠄊邉󠄈邊辺󠄁辺󠄁辺邉󠄕邉邉󠄁渡邊邊󠄍",
+                new NabeEncoder().Encode(src));
+
+        }
+
+        [Fact]
+        public void Encode009() {
+            const string src = "abcABC123あいう";
+            Assert.Equal("辺辺󠄀辺󠄁辺󠄂邉邉󠄀邉󠄁邉󠄂邊邊󠄐邊󠄑邊󠄒渡邊邊󠄓邊󠄓邉邉󠄆邉󠄂邊邊󠄓邊󠄓邉邉󠄆邉󠄄邊邊󠄓邊󠄓邉邉󠄆邉󠄆渡",
+                new NabeEncoder().Encode(src));
+        }
+    }
+
+    public class UnitTest3 {
+        [Fact]
+        public void Decode001() {
+            const string src = "辺辺󠄀辺󠄁辺󠄂";
+            Assert.Equal("abc", new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode002() {
+            const string src = "邉邉󠄀邉󠄁邉󠄂";
+            Assert.Equal("ABC", new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode003() {
+            const string src = "邊邊󠄐邊󠄑邊󠄒";
+            Assert.Equal("123", new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode004() {
+            const string src = "渡邊邊󠄓邊󠄓邉邉󠄆邉󠄂邊邊󠄓邊󠄓邉邉󠄆邉󠄄邊邊󠄓邊󠄓邉邉󠄆邉󠄆渡";
+            Assert.Equal("あいう", new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode005() {
+            const string src = "辺辺󠄀辺󠄁辺󠄂邉󠄀邉󠄁邉󠄂邉󠄃邉󠄄邉󠄅邉󠄆邉󠄇邉󠄈邉󠄉邉󠄊邉󠄋邉󠄌邉󠄍邉󠄎邉󠄏邉󠄐邉󠄑邉󠄒邉󠄓邉󠄔邉󠄕邉󠄖";
+            Assert.True(Enumerable.Range(0, 26).Select(x => (char) ('a' + x))
+                .Zip(new NabeDecoder().Decode(src), (a, b) => a == b).All(x => x));
+        }
+
+        [Fact]
+        public void Decode006() {
+            const string src = "邉邉󠄀邉󠄁邉󠄂邉󠄃邉󠄄邉󠄅邉󠄆邉󠄇邉󠄈邉󠄉邉󠄊邉󠄋邉󠄌邉󠄍邉󠄎邉󠄏邉󠄐邉󠄑邉󠄒邉󠄓邉󠄔邉󠄕邉󠄖邉󠄗邉󠄘邉󠄙";
+            Assert.True(Enumerable.Range(0, 26).Select(x => (char)('A' + x))
+                .Zip(new NabeDecoder().Decode(src), (a, b) => a == b).All(x => x));
+        }
+
+        [Fact]
+        public void Decode007() {
+            const string src = "邊邊󠄏邊󠄐邊󠄑邊󠄒邊󠄓邊󠄔辺󠄀辺󠄁辺󠄂邉󠄀";
+            Assert.Equal("0123456789", new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode008() {
+            const string src = "渡邊邊󠄓邊󠄓邉邉󠄆邉󠄓邊邊󠄓邊󠄓邉邉󠄊邉󠄓邊邊󠄓邊󠄓邉邉󠄆辺邉󠄎邊邊󠄓邊󠄓邉邉󠄆辺邉󠄄邊邊󠄓邊󠄓邉邉󠄆辺邉󠄒渡邊邊󠄀渡邊邊󠄓邊󠄓邉邉󠄆邉󠄓邊邊󠄓邊󠄓邉邉󠄊邉󠄌邊邊󠄓邊󠄓邉邉󠄆辺邉󠄒渡辺邉󠄊邉󠄋辺󠄂渡邊邊󠄓邊󠄓邉邉󠄊辺邉󠄖邊邊󠄓邊󠄓邉邉󠄎辺邉󠄁邊邊󠄓邊󠄓邉邉󠄎辺邉󠄖邊邊󠄓邊󠄓邉邉󠄎邉󠄉邊邊󠄓邊󠄓邉邉󠄆辺邉󠄑渡邉邉󠄓辺邉󠄁邉󠄏邉󠄐渡邊邊󠄓邊󠄓邉邉󠄆辺邉󠄊邊邊󠄓邊󠄓邉邉󠄆邉󠄙邊邊󠄓邊󠄓邉邉󠄊邉󠄈邊辺󠄁辺󠄁辺邉󠄕邉邉󠄁渡邊邊󠄍";
+            Assert.Equal(
+                "こんにちは!これはnocコマンドのTestですよ！.",
+                new NabeDecoder().Decode(src));
+        }
+
+        [Fact]
+        public void Decode009() {
+            const string src = "辺辺󠄀辺󠄁辺󠄂邉邉󠄀邉󠄁邉󠄂邊邊󠄐邊󠄑邊󠄒渡邊邊󠄓邊󠄓邉邉󠄆邉󠄂邊邊󠄓邊󠄓邉邉󠄆邉󠄄邊邊󠄓邊󠄓邉邉󠄆邉󠄆渡";
+            Assert.Equal("abcABC123あいう", new NabeDecoder().Decode(src));
+        }
+    }
+}
--- noc/noc/Program.cs
+++ noc/noc/Program.cs
@@ -4,34 +4,45 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 
-namespace NabeObfuscationCode
-{
-    internal class Program
-    {
-        private static void Main(string[] args)
-        {
-            var nabe = new Nabe();
+namespace noc {
+    internal class Program {
+        private static void Main(string[] args) {
 
-            if (args.Length != 0 && (args[0] == "-d" || args[0] == "--decode"))
-            {
-                var src = args.Length == 1 ? GetStringFromStdIn() : string.Join(" ", args.Skip(1));
-                Console.WriteLine(nabe.Decode(src).Trim());
+            if (args.Length != 0 && (args[0] == "-v" || args[0] == "--version")) {
+                Console.WriteLine(
+                    "noc - Nave Obfuscation Converter v1.1\nAuthor: xztaityozx\n Repo: https://github.com/xztaityozx/noc");
+                return;
             }
-            else
-            {
+
+
+            if (args.Length != 0 && (args[0] == "-h" || args[0] == "--help")) {
+                Console.WriteLine(
+                    "Usage: noc [option] [string]\n[string]をワタナベ難読化シェル芸を使って難読化します\n[string]を指定しない場合，StdInから読み取られます");
+                Console.WriteLine("[option]\n  -d --decode\t デコードします");
+                Console.WriteLine("  -h --help\t ヘルプを出力して終了します");
+                Console.WriteLine("  -v --version\t バージョン情報を出力して終了します");
+                return;
+            }
+
+
+
+            if (args.Length != 0 && (args[0] == "-d" || args[0] == "--decode")) {
+                var decoder = new NabeDecoder();
+                var src = args.Length == 1 ? GetStringFromStdIn() : string.Join(" ", args.Skip(1));
+                Console.WriteLine(decoder.Decode(src).Trim());
+            }
+            else {
+                var encoder = new NabeEncoder();
                 var src = args.Length == 0 ? GetStringFromStdIn() : string.Join(" ", args);
-                Console.WriteLine(nabe.Encode(src).Trim());
+                Console.WriteLine(encoder.Encode(src).Trim());
             }
 
         }
 
-        private static string GetStringFromStdIn()
-        {
+        private static string GetStringFromStdIn() {
             var sb = new StringBuilder();
             string line;
-            Console.InputEncoding = Encoding.UTF8;
-            while ((line = Console.ReadLine()) != null)
-            {
+            while ((line = Console.ReadLine()) != null) {
                 sb.AppendLine(line);
             }
 
@@ -40,33 +51,185 @@ namespace NabeObfuscationCode
     }
 
 
+    public class NabeEncoder : Nabe {
 
-    public class Nabe
-    {
-        public class Block
-        {
-            public enum Mode
-            {
+
+        private List<Block> EncodeSplit(string str) {
+            var rt = new List<Block>();
+
+            var currentBlock = new Block();
+            for (var item = StringInfo.GetTextElementEnumerator(str); item.MoveNext();) {
+                var nextMode = GetMode(item.GetTextElement());
+                if (currentBlock.BlockMode == Block.Mode.Any) {
+                    currentBlock.BlockMode = nextMode;
+                    currentBlock.Message += item.GetTextElement();
+                }
+                else if (currentBlock.BlockMode == nextMode) {
+                    currentBlock.Message += item.GetTextElement();
+                }
+                else {
+                    rt.Add(currentBlock);
+                    currentBlock = new Block {BlockMode = nextMode, Message = item.GetTextElement()};
+                }
+            }
+
+            rt.Add(currentBlock);
+
+            return rt;
+        }
+
+        private Dictionary<Block.Mode, Dictionary<string, string>> inverseTable;
+
+        private void BuildInverseTable() {
+            inverseTable = new Dictionary<Block.Mode, Dictionary<string, string>>();
+            foreach (var table in ConvertTable) {
+                var key = table.Key;
+                var invCollection = new Dictionary<string, string>();
+                foreach (var item in table.Value) {
+                    invCollection.Add(item.Value, item.Key);
+                }
+
+                inverseTable.Add(key, invCollection);
+            }
+        }
+
+
+        public string Encode(string str) {
+
+            BuildInverseTable();
+
+            var list = new List<Block>();
+
+            string Filter(Block block) {
+                var filtered = Block.InverseDictionary[block.BlockMode];
+                for (var item = StringInfo.GetTextElementEnumerator(block.Message); item.MoveNext();) {
+                    filtered += inverseTable[block.BlockMode][item.GetTextElement()];
+                }
+
+                return filtered;
+            }
+
+            foreach (var b in EncodeSplit(str)) {
+                if (b.BlockMode == Block.Mode.Base64) {
+                    var b64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(b.Message));
+                    list.Add(new Block {BlockMode = Block.Mode.Base64, Message = ""});
+                    list.AddRange(EncodeSplit(b64));
+                    list.Add(new Block {BlockMode = Block.Mode.Base64, Message = ""});
+                }
+                else {
+                    list.Add(b);
+                }
+            }
+
+
+            return string.Join("", list.Select(Filter));
+        }
+    }
+
+    public class NabeDecoder : Nabe {
+
+        private List<Block> DecodeSplit(string str) {
+            var rt = new List<Block>();
+
+            var block = new Block {BlockMode = Block.Mode.Any, Message = ""};
+
+            for (var item = StringInfo.GetTextElementEnumerator(str); item.MoveNext();) {
+                var element = item.GetTextElement();
+                if (block.BlockMode == Block.Mode.Any || Block.IsDelimiter(element)) {
+                    if (block.BlockMode != Block.Mode.Any) rt.Add(block);
+                    block = new Block {
+                        BlockMode = element == Block.InverseDictionary[Block.Mode.Base64] ? Block.Mode.Base64 :
+                            element == Block.InverseDictionary[Block.Mode.Big] ? Block.Mode.Big :
+                            element == Block.InverseDictionary[Block.Mode.Small] ? Block.Mode.Small :
+                            Block.Mode.Symbol
+                    };
+                }
+                else {
+                    block.Message += element;
+                }
+            }
+
+            rt.Add(block);
+
+            return rt;
+        }
+
+        private string Filter(Block block) {
+            var rt = "";
+
+            for (var item = StringInfo.GetTextElementEnumerator(block.Message); item.MoveNext();) {
+                rt += ConvertTable[block.BlockMode][item.GetTextElement()];
+            }
+
+            return rt;
+        }
+
+        public string Decode(string str) {
+            var rt = "";
+
+            var src = DecodeSplit(str);
+
+
+            var idx = 0;
+            for (; idx < src.Count;) {
+                var block = src[idx];
+                if (block.BlockMode == Block.Mode.Base64) {
+                    idx++;
+                    var b64 = Filter(src[idx]);
+                    while (src[idx++].BlockMode != Block.Mode.Base64) {
+                        b64 += Filter(src[idx]);
+                    }
+
+                    rt += Encoding.UTF8.GetString(Convert.FromBase64String(b64));
+                    idx--;
+                }
+                else {
+                    rt += Filter(block);
+                }
+
+                idx++;
+            }
+
+            return rt;
+        }
+
+    }
+
+    public class Nabe {
+        public class Block {
+
+            public Block() {
+                BlockMode = Mode.Any;
+                Message = "";
+            }
+
+            protected bool Equals(Block other) {
+                return string.Equals(Message, other.Message) && BlockMode == other.BlockMode;
+            }
+
+            public override string ToString() {
+                return $"Mode: {BlockMode}, Message: {Message}";
+            }
+
+            public enum Mode {
                 Small,
                 Big,
                 Symbol,
+                Base64,
                 Any
             }
 
-            public static readonly Dictionary<string, Mode> ModeDictionary = new Dictionary<string, Mode> {
-                {"辺", Mode.Small},
-                {"邉", Mode.Big},
-                {"邊", Mode.Symbol},
-            };
 
             public static readonly Dictionary<Mode, string> InverseDictionary = new Dictionary<Mode, string> {
                 {Mode.Small, "辺"},
                 {Mode.Big, "邉"},
                 {Mode.Symbol, "邊"},
-                {Mode.Any, " "}
+                {Mode.Base64, "渡"},
             };
 
-            public static bool IsDelimiter(string str) => new[] { "辺", "邊", "邉" }.Contains(str);
+            public static bool IsDelimiter(string str) => new[] {"辺", "邊", "邉", "渡"}.Contains(str);
+            public string Message { get; set; }
+            public Mode BlockMode { get; set; }
         }
 
         public readonly Dictionary<Block.Mode, Dictionary<string, string>> ConvertTable =
@@ -186,68 +349,19 @@ namespace NabeObfuscationCode
             "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", " "
         };
 
-        public string Decode(string str)
-        {
-            var rt = "";
-            var currentMode = Block.Mode.Any;
-            for (var item = StringInfo.GetTextElementEnumerator(str); item.MoveNext();)
-            {
 
-                if (Block.IsDelimiter(item.GetTextElement()))
-                {
-                    currentMode = Block.ModeDictionary[item.GetTextElement()];
+        public Block.Mode GetMode(string str) {
+            var idx = 0;
+            while (idx<targets.Length && targets[idx] != str) if(idx<targets.Length) idx++;
+                else {
+                    idx = -1;
+                    break;
                 }
-                else
-                {
-                    if (currentMode == Block.Mode.Any) rt += item.GetTextElement();
-                    else if (ConvertTable[currentMode].ContainsKey(item.GetTextElement())) rt += ConvertTable[currentMode][item.GetTextElement()];
-                    else rt += item.GetTextElement();
-                }
-            }
-
-            return rt;
+            return 0 <= idx && idx <= 25 ? Block.Mode.Small :
+                26 <= idx && idx <= 51 ? Block.Mode.Big :
+                52 <= idx && idx < targets.Length ? Block.Mode.Symbol : Block.Mode.Base64;
         }
 
-        public string Encode(string str)
-        {
-            var rt = "";
-
-            var currentMode = Block.Mode.Any;
-
-            var inverseTable = new Dictionary<Block.Mode, Dictionary<string, string>>();
-            foreach (var table in ConvertTable)
-            {
-                var key = table.Key;
-                var invCollection = new Dictionary<string, string>();
-                foreach (var item in table.Value)
-                {
-                    invCollection.Add(item.Value, item.Key);
-                }
-
-                inverseTable.Add(key, invCollection);
-            }
-
-            foreach (var item in str.Select(x => x.ToString()))
-            {
-                if (targets.Contains(item))
-                {
-                    var idx = 0;
-                    while (item != targets[idx]) idx++;
-                    var nextMode = idx <= 25 ? Block.Mode.Small : idx <= 51 ? Block.Mode.Big : Block.Mode.Symbol;
-
-                    if (nextMode != currentMode) rt += Block.InverseDictionary[nextMode];
-                    currentMode = nextMode;
-
-                    rt += inverseTable[currentMode][item];
-                }
-                else
-                {
-                    rt += item;
-                }
-            }
-
-            return rt;
-        }
 
     }
 
